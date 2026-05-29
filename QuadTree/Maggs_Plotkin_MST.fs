c0dcef16ce6d6a4348c50bd216bd5d431b96349f
gsv <rsdpisuy@gmail.com>
2026-05-29 12:45:43 +0300

    Formatted

--- QuadTree/Maggs_Plotkin_MST.fs
+++ QuadTree/Maggs_Plotkin_MST.fs
@@ -12,48 +12,59 @@ type Error =
 
 let mst (graph: Matrix.SparseMatrix<'a>) =
 
-    let diag = 
+    let diag =
         let zero = Unchecked.defaultof<'a>
+
         Matrix.fromCoordinateList (
             Matrix.CoordinateList(
                 graph.nrows,
                 graph.ncols,
-                [ for i in 0UL .. uint64 graph.nrows -> (i * 1UL<rowindex>, i * 1UL<colindex>, zero)]
+                [ for i in 0UL .. uint64 graph.nrows -> (i * 1UL<rowindex>, i * 1UL<colindex>, zero) ]
             )
         )
-    let _max x y = 
-        match (x,y) with
+
+    let _max x y =
+        match (x, y) with
         | Some x, Some y -> max x y |> Some
         | _ -> None
 
-    let _min x y = 
-        match (x,y) with
+    let _min x y =
+        match (x, y) with
         | Some x, Some y -> min x y |> Some
-        | Some x , None
+        | Some x, None
         | None, Some x -> Some x
         | _ -> None
 
     resultM {
-        let! graph = 
-            Matrix.map2 graph diag (fun x y -> match y with None -> x | _ -> y)
+        let! graph =
+            Matrix.map2 graph diag (fun x y ->
+                match y with
+                | None -> x
+                | _ -> y)
             |> Result.mapError DiagAdditionProblem
-        
-        let! closure = 
-            let rec compute (matrix:SparseMatrix<_>) =
+
+        let! closure =
+            let rec compute (matrix: SparseMatrix<_>) =
                 let nnz = matrix.nvals
-                resultM{
 
-                    let! step = LinearAlgebra.mxm _min _max matrix matrix 
-                    if nnz = step.nvals && matrix.storage = step.storage
-                    then return step
-                    else return! compute step
+                resultM {
+
+                    let! step = LinearAlgebra.mxm _min _max matrix matrix
+
+                    if nnz = step.nvals && matrix.storage = step.storage then
+                        return step
+                    else
+                        return! compute step
                 }
-            compute graph
-            |> Result.mapError ClosureComputationProblem
 
-        let! mst = 
-            Matrix.map2i graph closure (fun i j x y -> if uint64 i = uint64 j then None else if x = y then x else None)
+            compute graph |> Result.mapError ClosureComputationProblem
+
+        let! mst =
+            Matrix.map2i graph closure (fun i j x y ->
+                if uint64 i = uint64 j then None
+                elif x = y then x
+                else None)
             |> Result.mapError MSTComputationProblem
+
         return mst
     }
-
